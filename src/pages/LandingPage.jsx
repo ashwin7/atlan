547ba4f63e9ea63ddde66d24c27069afcd9381f8
Ashwin Saxena <ashwinsaxena7@gmail.com>
2024-04-26 18:06:55 +0530

    Removed video tag

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,12 +6,6 @@ import "font-awesome/css/font-awesome.min.css";
 export default function LandingPage() {
   return (
     <div>
-      <div className="video-bg">
-        <video width="320" height="240" autoPlay loop muted>
-          <source src="https://assets.codepen.io/3364143/7btrrd.mp4" type="video/mp4"></source>
-            Your browser does not support the video tag.
-        </video>
-      </div>
       <Header />
       <Editor />
       <Footer />
